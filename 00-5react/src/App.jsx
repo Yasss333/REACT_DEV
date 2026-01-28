02e3babb929723f0d2cfa93699f3256cf5c79554
Yasss333 <yashmandhare3@gmail.com>
2026-01-28 15:21:00 +0530

    usesatet , useefectdeone

--- 00-5react/src/App.jsx
+++ 00-5react/src/App.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
+
+  const [quote, setQuote]=useState("Old is gold ")
+
+
+   useState(()=>{
+       fetch("https://api.freeapi.app/api/v1/public/quotes/quote/random")
+       .then((res)=>res.json())
+       .then((mahiti)=>setQuote(mahiti.data.content
+       ))
+       .catch((e)=>setQuote(e))
+   },[])
+
   return (
     <div>
       <h1>Hey </h1>
       <h6>Yash here </h6>
+      <h3>{quote}</h3>
     </div>
   )
 }
